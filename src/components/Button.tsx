096b4eb40d8ae861a6fe7770aa6ae48c607bd0ab
Mário César Silva <mcgato33@hotmail.com>
2021-04-20 16:28:58 -0100

    Confirmation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
-import { 
+import {
   TouchableOpacity,
   Text,
-  TouchableOpacityProps, 
-  StyleSheet
+  StyleSheet,
+  TouchableOpacityProps
 } from 'react-native';
 
-import colors from '../styles/colors';
-import fonts from '../styles/fonts';
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
-interface ButtonProps extends TouchableOpacityProps{
+interface ButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
-export function Button() {
-  return (
-    <TouchableOpacity style={styles.container}>
-      <Text style={styles.text}>Confirmar</Text>
+export function Button({ title, ...rest }: ButtonProps ){
+  return(
+    <TouchableOpacity 
+    style={styles.container}
+    {...rest}
+    >
+      <Text style={styles.text}>
+        {title}
+      </Text>
     </TouchableOpacity>
-  );
+  )
 }
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.green,
-    height: 56,
+    height: 56, 
     borderRadius: 16,
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   text: {
     fontSize: 16,
     color: colors.white,
-    fontFamily: fonts.heading,
+    fontFamily: fonts.heading
   }
-});
+})
