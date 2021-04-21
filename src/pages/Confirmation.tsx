874825fc3dda903d96ed7232fd7b6794962f8198
Mário César Silva <mcgato33@hotmail.com>
2021-04-21 11:24:08 -0100

    create PlantSelect

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/core";
 
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
@@ -8,6 +9,12 @@ import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -21,7 +28,8 @@ export function Confirmation() {
 
         <View style={styles.footer}>
           <Button 
-          title="Começar"
+            title="Começar"
+            onPress={handleMoveOn}
           />
         </View>
       </View>
